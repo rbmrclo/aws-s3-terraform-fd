db3c76d77897caf32ddb65def4847fe072848374
Robbie Marcelo <rbmrclo@hotmail.com>
2019-04-08 14:03:40 +0800

    Remove s3 bucket

--- main.tf
+++ main.tf
@@ -9,31 +9,3 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   max_retries = 3
 }
-
-resource "aws_s3_bucket" "public_website" {
-  bucket = "robbie-test-force-destroy"
-  acl    = "private"
-  website = {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
-}
-
-resource "aws_s3_bucket_policy" "public_website" {
-  bucket = "${aws_s3_bucket.public_website.id}"
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AddPerm",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::robbie-test-force-destroy/*"
-    }
-  ]
-}
-POLICY
-}
-
